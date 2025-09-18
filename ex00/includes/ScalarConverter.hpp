8a6bca88c32be8c40b7f89af98a5503cd18d7f33
Nofy261 <noufy33@yahoo.com>
2025-09-18 13:25:15 +0200

    first commit

---
+++ ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 13:17:30 by nolecler          #+#    #+#             */
+/*   Updated: 2025/09/18 13:22:16 by nolecler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class ScalarConverter
+{
+    public :
+    
+        static void convert(const std::string &type);
+
+    
+    private :
+
+  
+};
