90d2a55bbbb3ed2c073025351edd131ed2ba5986
Nofy261 <noufy33@yahoo.com>
2025-09-22 12:04:41 +0200

    in progress

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 13:17:34 by nolecler          #+#    #+#             */
-/*   Updated: 2025/09/19 12:22:02 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/09/22 11:13:29 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,7 @@ int main(int argc, char **argv)
     // verifier si chaine vide ? ./convert " "
     if (argc == 2)
     {
-        ScalarConverter::convert(argv[1]);
-
-
-
-        
+        ScalarConverter::convert(argv[1]); 
     }
     else
         std::cout << "Invalid number of argument." << std::endl;
