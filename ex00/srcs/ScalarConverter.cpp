b480f46891610da6dac3ad057379a37c6a21c3c8
Nofy261 <noufy33@yahoo.com>
2025-09-26 13:03:11 +0200

    first exo is finished and tested

--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -6,17 +6,17 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 13:17:37 by nolecler          #+#    #+#             */
-/*   Updated: 2025/09/25 14:13:37 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/09/26 13:01:58 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
-#include <cstdlib> //atof
+#include <cstdlib>
 #include <iomanip>
 #include <limits>
 #include <sstream>
-#include <cerrno> // ernno strtol
+#include <cerrno>
 
 
 static bool isPseudoLiteral(const std::string &str) 
@@ -25,7 +25,7 @@ static bool isPseudoLiteral(const std::string &str)
     {
         std::cout << "char: impossible" << std::endl;
         std::cout << "int: impossible" << std::endl;
-        std::cout << "float: " << str << "f" << std::endl; // a voir si c'est accepter d'ecrire manuellement
+        std::cout << "float: " << str << "f" << std::endl;
         std::cout << "double: " << str << std::endl;
         return true;
     }
@@ -47,30 +47,19 @@ static bool isChar(const std::string &str)
     return false;
 }
 
-
-//long int strtol (const char* str, char** endptr, int base);
-// gerer les espaces et/ou tab avant Ex:"   77880"
-
 bool isInt(const std::string &str)
 {
-    // if (!isdigit(str[0]) && str[0] != '+' && str[0] != '-')
-    //     return false;
-    
-    // for(int i = 1; i < str.size(); i++)
-    // {
-    //     if (!isdigit(str[i]))
-    //         return false;
-    // }
-    
-   // convertir en long
     char *end;
     errno = 0;
     long int c = strtol(str.c_str(), &end, 10);
     
-    if (*end != '\0') // strtol n'a pas lu la chaine jusqu a la fin car caractere invalide trouvé  Ex: 123adc 
-        return false;
-    
-    if (errno == ERANGE) // si overflow d'un long int
+    while (*end != '\0')
+    {
+        if (!std::isspace(static_cast<unsigned char>(*end)))
+            return false;
+        end++;
+    }  
+    if (errno == ERANGE)
     {
         std::cout << "Error 1: Value is invalid ." << std::endl;
         return false;
@@ -84,87 +73,72 @@ bool isInt(const std::string &str)
     return true;
 }
 
+// float x = .5f == 0.5f CAS VALIDE A GERER OU PAS
 
- 
-
-
-// float x = .5f == 0.5f CAS VALIDE A GERER 
-// ./convert .5f cas a gerer ?? 
-// a tester ++ -- +. -.
-// il faut gerer les espace et tab au debut Ex : ./convert "  +234f" ce n'est pas gerer ici
 static bool isFloat(const std::string &str)
 {
-    
-    //int i = 0;
     if (str.size() <= 1)
         return false;
     if (str[str.length() - 1] != 'f')
         return false;
-    // while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-	// 	i++;
-    if (!isdigit(str[0]) && str[0] != '+' && str[0] != '-')
-    {
-        std::cout << "Error 1: Invalid float" << std::endl;
+
+    unsigned long int i = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;    
+    if (!isdigit(str[i]) && str[i] != '+' && str[i] != '-')
         return false;
-    }
- 
-    std::string s = str.substr(0, str.length() - 1); // on enleve le f en dernier
+    i++;
+    if ((str[i - 1] == '+' || str[i - 1] == '-') && (!isdigit(str[i])))
+        return false;
+    if (isdigit(str[i - 1]) && (str[i] != '.' && !isdigit(str[i])))
+        return false;
+
+    std::string s = str.substr(0, str.length() - 1);
        
-    int count_dot = 0;   
-    for (unsigned long int i = 1; i < s.length(); i++) // on commence a 1 car on a defa validé ou pas le 1er caractere
+    unsigned long int count_dot = 0;
+    while (i < s.size())
     {
         if (s[i] == '.')
-            count_dot++;       
-        if (count_dot > 1)
+            count_dot++;
+        if (s[i] != '.' && !isdigit(s[i]))
             return false;
-        if (!isdigit(s[i]) && s[i] != '.') // && str[0] != '+' && str[0] != '-')
-        {
-            std::cout << "Error 2: Invalid float" << std::endl;
-            return false;
-        }
+        i++;
     }
+    if (count_dot > 1)
+        return false;
     return true; 
 }
 
 
-// s'il y a un point mais pas de f
-// a gerer +. ou -. ou ++ -- invalide
-// il faut gerer les espace et tab au debut Ex : ./convert "  +234f"
 static bool isDouble(const std::string &str)
 {
     if (str.size() <= 1)
         return false;
-    // int i = 0;
-    // while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-	// 	i++;
-    if (!isdigit(str[0]) && str[0] != '+' && str[0] != '-')
-    {
-        std::cout << "Error 1: Invalid double" << std::endl;
+
+    unsigned long int i = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;    
+    if (!isdigit(str[i]) && str[i] != '+' && str[i] != '-')
         return false;
-    }
-    
-    if ((str[0] == '+' || str[0] == '-') && (!isdigit(str[1])))
-    {
-        std::cout << "Error 2: Invalid double" << std::endl; // ./convert ++kl a tester
+    i++;
+    if ((str[i - 1] == '+' || str[i - 1] == '-') && (!isdigit(str[i])))
         return false;
-    }
+    if (isdigit(str[i - 1]) && (str[i] != '.' && !isdigit(str[i])))
+        return false;
     
-    int count_dot = 0;   
-    for (unsigned long int i = 1; i < str.size(); i++) // on commence a 1 car on a defa validé ou pas le 1er caractere
+    unsigned long int count_dot = 0;
+    while (i < str.size())
     {
         if (str[i] == '.')
-            count_dot++;       
-        if (!isdigit(str[i]) && str[i] != '.')
-        {
-            std::cout << "Error 3: Invalid double" << std::endl; // ./convert +85j  a tester
+            count_dot++;
+        if (str[i] != '.' && !isdigit(str[i]))
             return false;
-        }
+        i++;
     }
     if (count_dot != 1)
         return false;
     return true; 
 }
-
 
 void ScalarConverter::convert(const std::string &str)
 { 
@@ -180,7 +154,7 @@ void ScalarConverter::convert(const std::string &str)
             std::cout << "float: " << static_cast<float>(str[0]) << ".0f" << std::endl;
             std::cout << "double: " << static_cast<double>(str[0]) << ".0" << std::endl;
         }
-        else if (!isprint(str[0])) // a tester tout les cas
+        else if (!isprint(str[0]))
         {
             std::cout << "char: Non displayable" << std::endl;
             std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
@@ -188,7 +162,7 @@ void ScalarConverter::convert(const std::string &str)
             std::cout << "double: " << static_cast<double>(str[0]) << ".0" << std::endl;
         }
     }
-    if (isInt(str))
+    else if (isInt(str))
     {
         int c = atoi(str.c_str());
         
@@ -205,8 +179,9 @@ void ScalarConverter::convert(const std::string &str)
     
     else if (isFloat(str))
     {
-        float f = atof(str.c_str());
+        std::string s = str.substr(0, str.length() - 1); 
         
+        float f = atof(s.c_str());
         if (f < 0 || f > 127)
             std::cout << "char: impossible" << std::endl; 
         else if (f < 32 || f == 127)
@@ -214,7 +189,7 @@ void ScalarConverter::convert(const std::string &str)
         else      
             std::cout << "char: " << static_cast<char>(f) << std::endl;
             
-        std::stringstream ssd(str);
+        std::stringstream ssd(s);
         double d;
         ssd >> d;
         
@@ -226,7 +201,7 @@ void ScalarConverter::convert(const std::string &str)
         std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(f) << "f" <<std::endl;
         std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(f) << std::endl;
     }
-    else if (isDouble(str)) // A TESTER TOUS LES CAS
+    else if (isDouble(str))
     {
         std::stringstream ssd(str);
         double d;
@@ -247,9 +222,11 @@ void ScalarConverter::convert(const std::string &str)
         std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(d) <<"f"<<std::endl;
         std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
     }
-    // else // verifier dans le main ?
-    // {
-    //     std::cout << "Invalid literal." << std::endl;
-    //     // pas d argument ou trop d argument donné 
-    // }
+    else
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: impossible" << std::endl;
+        std::cout << "double: impossible" << std::endl;
+    }
 } 
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,27 +6,26 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 13:17:34 by nolecler          #+#    #+#             */
-/*   Updated: 2025/09/25 11:14:49 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/09/26 13:02:40 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
 
-
 int main(int argc, char **argv)
 {
-    // A verifier ici si chaine vide  ./convert " "
-    
     if (argc == 2)
     {
-        // if (argv[0].empty)
-        //     return (1);
+        std::string arg = argv[1];
+        if (arg.empty())
+        {
+            std::cout << "Value is empty." << std::endl;
+            return (1);
+        }
         ScalarConverter::convert(argv[1]); 
     }
     else
         std::cout << "Invalid number of argument." << std::endl;
     return (0);
 }
-
-
