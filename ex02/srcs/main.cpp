3c0dc20d1f3c38ef91ba09bce37d8e1373a072c7
Nofy261 <noufy33@yahoo.com>
2025-09-27 14:54:01 +0200

    preparation for last exo

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/27 14:34:31 by nolecler          #+#    #+#             */
+/*   Updated: 2025/09/27 14:53:30 by nolecler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <exception>
+#include "time.h"
+
+
+
+
+
+Base * generate(void)
+{
+    
+}
+
+
+void identify(Base* p)
+{
+    
+}
+
+void identify(Base& p)
+{
+    
+}
+
+
+int main()
+{
+    
+}
