d1211c16c593e39c62ab40d5de2f26d32b4e8cd8
Nofy261 <noufy33@yahoo.com>
2025-10-24 10:07:10 +0200

    good version

--- ex00/srcs/ScalarConverter.cpp
+++ ex00/srcs/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 13:17:37 by nolecler          #+#    #+#             */
-/*   Updated: 2025/09/30 16:57:13 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/10/02 11:33:17 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 #include <iomanip>
 #include <limits>
 #include <climits> 
-#include <sstream>
 #include <cerrno>
+#include <cctype>
 
 ScalarConverter::ScalarConverter(){}
 
@@ -57,23 +57,28 @@ static bool isPseudoLiteral(const std::string &str)
 
 static bool isChar(const std::string &str)
 {
-    if (str.length() == 1 && !isdigit(str[0]))
+    if (str.length() == 1 && !std::isdigit(str[0]))
         return true; 
     return false;
 }
 
 bool isInt(const std::string &str)
 {
-    char *end;
+    unsigned long int i = 1;
     errno = 0;
-    long int c = strtol(str.c_str(), &end, 10);
+    long int c = strtol(str.c_str(), NULL, 10);
     
-    while (*end != '\0')
+    if (!std::isdigit(str[0]) && str[0] != '+' && str[0] != '-')
+        return false;
+    if ((str[0] == '+' || str[0] == '-') && !std::isdigit(str[1]))
+        return false;
+    
+    while (i < str.size())
     {
-        if (!std::isspace(static_cast<unsigned char>(*end)))
+        if (!isdigit(str[i]))
             return false;
-        end++;
-    }  
+        i++;
+    }
     if (errno == ERANGE)
         return false;
     if (c > INT_MAX || c < INT_MIN)
@@ -88,17 +93,11 @@ static bool isFloat(const std::string &str)
         return false;
     if (str[str.length() - 1] != 'f')
         return false;
-    unsigned long int i = 0;
-    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-		i++;    
-    if (!isdigit(str[i]) && str[i] != '+' && str[i] != '-')
+    unsigned long int i = 1;
+    if (!std::isdigit(str[0]) && str[0] != '+' && str[0] != '-')
         return false;
-    i++;
-    if ((str[i - 1] == '+' || str[i - 1] == '-') && (!isdigit(str[i])))
-        return false;
-    if (isdigit(str[i - 1]) && (str[i] != '.' && !isdigit(str[i])))
-        return false;
-
+    if ((str[0] == '+' || str[0] == '-') && !std::isdigit(str[1]))
+        return false; 
     std::string s = str.substr(0, str.length() - 1);
        
     unsigned long int count_dot = 0;
@@ -120,16 +119,12 @@ static bool isDouble(const std::string &str)
 {
     if (str.size() <= 1)
         return false;
-    unsigned long int i = 0;
-    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-		i++;    
-    if (!isdigit(str[i]) && str[i] != '+' && str[i] != '-')
+    unsigned long int i = 1;
+    
+    if (!std::isdigit(str[0]) && str[0] != '+' && str[0] != '-')
         return false;
-    i++;
-    if ((str[i - 1] == '+' || str[i - 1] == '-') && (!isdigit(str[i])))
-        return false;
-    if (isdigit(str[i - 1]) && (str[i] != '.' && !isdigit(str[i])))
-        return false;
+    if ((str[0] == '+' || str[0] == '-') && !std::isdigit(str[1]))
+        return false; 
     unsigned long int count_dot = 0;
     while (i < str.size())
     {
@@ -139,23 +134,17 @@ static bool isDouble(const std::string &str)
             return false;
         i++;
     }
-    
-    std::stringstream ssd(str);
-    double d;
-    ssd >> d;
+    double d = strtod(str.c_str(), NULL);
     
     if (count_dot > 1)
         return false;
     if (count_dot == 0 && ((d > std::numeric_limits<int>::max()) || (d < std::numeric_limits<int>::min())))
         return true;
-    if (count_dot == 0)
-        return false; 
     return true; 
 }
 
-
 void ScalarConverter::convert(const std::string &str)
-{ 
+{
     if (isPseudoLiteral(str))
         return;
     else if (isChar(str))
@@ -167,7 +156,7 @@ void ScalarConverter::convert(const std::string &str)
             std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(str[0]) << "f" <<std::endl;
             std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(str[0]) << std::endl;
         }
-        else if (!isprint(str[0]))
+        else
         {
             std::cout << "char: Non displayable" << std::endl;
             std::cout << "int: " << static_cast<int>(str[0]) << std::endl;
@@ -201,46 +190,24 @@ void ScalarConverter::convert(const std::string &str)
         else
             std::cout << "char: " << "'" << static_cast<char>(f) << "'" << std::endl;
         
-        std::stringstream ssd(s);
-        double d;
-        ssd >> d;
-        
+        double d = strtod(s.c_str(), NULL);
         if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min())
             std::cout << "int: impossible" << std::endl; 
         else
             std::cout << "int: " << static_cast<int>(d) << std::endl;
-        std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(f) << "f" <<std::endl;
-        //std::cout << "float: " <<  f <<std::endl;
-        std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(f) << std::endl;
+
+        if (f == std::numeric_limits<float>::infinity())
+            std::cout << "float: +inff" << std::endl;
+        else if (f == -std::numeric_limits<float>::infinity())
+            std::cout << "float: -inff" << std::endl;
+        else
+            std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f" <<std::endl;
+        std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
     }
-    // else if (isDouble(str))
-    // {
-    //     std::stringstream ssd(str);
-    //     double d;
-    //     ssd >> d;
-
-    //     if (d < 0 || d > 127)
-    //         std::cout << "char: impossible" << std::endl; 
-    //     else if (d < 32 || d == 127)
-    //         std::cout << "char: Non displayable" << std::endl;
-    //     else      
-    //         std::cout << "char: " << "'" << static_cast<char>(d) << "'" << std::endl;
-
-    //     if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min())
-    //         std::cout << "int: impossible" << std::endl; 
-    //     else
-    //         std::cout << "int: " << static_cast<int>(d) << std::endl;
-
-    //     std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(d) <<"f"<<std::endl;
-    //     std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(d) << std::endl;
-    // }
-
+  
     else if (isDouble(str))
     {
-        std::stringstream ssd(str);
-        double d;
-        ssd >> d;
-
+        double d = strtod(str.c_str(), NULL);
         if (d < 0 || d > 127)
             std::cout << "char: impossible" << std::endl; 
         else if (d < 32 || d == 127)
@@ -253,15 +220,14 @@ void ScalarConverter::convert(const std::string &str)
         else
             std::cout << "int: " << static_cast<int>(d) << std::endl;
 
-        std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(d) <<"f"<<std::endl;
+        std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(d) << "f" <<std::endl;
 
-        if (ssd.fail())
-            std::cout << "double: impossible" << std::endl;
-        else if (d != d || d == std::numeric_limits<double>::infinity() || d == -std::numeric_limits<double>::infinity())
-            std::cout << "double: impossible" << std::endl;
+        if (d == std::numeric_limits<double>::infinity())
+            std::cout << "double: +inf" << std::endl;
+        else if (d == -std::numeric_limits<double>::infinity())
+            std::cout << "double: -inf" << std::endl;
         else
-            //std::cout << "double: " << std::scientific << d << std::endl;
-            std::cout << "double: " << d << std::endl;
+            std::cout << "double: " << std::fixed << std::setprecision(1) << d << std::endl;
     }
     else
     {
@@ -271,5 +237,3 @@ void ScalarConverter::convert(const std::string &str)
         std::cout << "double: impossible" << std::endl;
     }
 }
-
-//179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368
--- ex01/includes/Serializer.hpp
+++ ex01/includes/Serializer.hpp
@@ -6,24 +6,19 @@
 /*   By: nolecler <nolecler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 13:09:00 by nolecler          #+#    #+#             */
-/*   Updated: 2025/09/27 13:30:20 by nolecler         ###   ########.fr       */
+/*   Updated: 2025/10/02 11:52:51 by nolecler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once 
-
 #include <iostream>
 #include <stdint.h>
-
-// On sérialise un pointeur (Data* → uintptr_t)
-// on convertit un pointeur en un entier non signé puis on deserialise
 
 typedef struct Data
 {
     std::string name;
     int age;
 }       Data;
-
 
 class Serializer
 {
